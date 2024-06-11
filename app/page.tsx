e92bd6a07ecfbe98572512d7f1a63de9be1d97c3
jean-luc <jchoiseul@ufl.edu>
2024-06-11 19:15:35 -0400

    forgot to run build

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <p>This is Big J Moolah's web site! Big J Money!!</p>
+      <p>This is Big J Moolahs web site! Big J Money!!</p>
     </main>
   );
 }
